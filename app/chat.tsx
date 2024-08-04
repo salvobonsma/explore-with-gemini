dac4e6345fae29e9d5cfbb425f7b0c73b1ff8e76
ampersanded <salvobonsma@gmail.com>
2024-08-03 19:57:01 -0700

    chat

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,6 +1,7 @@
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {SendIcon} from "lucide-react";
+import {cn} from "@/lib/utils";
 
 export default function Chat({window, setWindow}: {
     window?: { lat: number, lng: number, zoom: number },
@@ -18,8 +19,19 @@ export default function Chat({window, setWindow}: {
           </div>
     );
 
+    function message(message: string, side: "left" | "right") {
+        return (
+              <div className={cn(side == "left" ? "self-start text-left" : "self-end text-right", "w-1/2 w- rounded-2xl p-4 bg-muted text-wrap")}>{message}</div>
+        )
+    }
+
     return (
-          <div className={"blue-shadow border rounded-2xl m-6 mb-0 lg:ml-0 lg:mb-6 aspect-square flex flex-col justify-end p-4"}>
+          <div className={"blue-shadow border rounded-2xl m-6 mb-0 lg:ml-0 lg:mb-6 aspect-square flex flex-col justify-end p-4 gap-4"}>
+              <div className={"flex-1 overflow-y-scroll flex flex-col gap-4 justify-end"}>
+                  {message("testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest", "right")}
+                  {message("test", "left")}
+                  {message("test", "right")}
+              </div>
               <div className={"flex gap-2"}>
                   <Input placeholder={"Ask me anything..."}/>
                   <Button className={"w-10 aspect-square p-2.5"}><SendIcon/></Button>
