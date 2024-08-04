6e71742406d0558ceb6085e64216cb3f284a0cd0
ampersanded <salvobonsma@gmail.com>
2024-08-04 10:52:22 -0700

    map

--- app/map.tsx
+++ app/map.tsx
@@ -22,26 +22,28 @@ export default function Map({window, setWindow}: {
                     backgroundColor={"bg-background"}
                     mapTypeId={mapType}
                     disableDefaultUI={true}
-              />
-              <MapControl position={1}>
-                  <div className={"rounded-lg bg-background m-4 p-1 outline outline-1 outline-accent"}>
-                      <Tabs defaultValue={mapType}>
-                          <TabsList>
-                              <TabsTrigger value="roadmap" onClick={() => setMapType("roadmap")}>Roadmap</TabsTrigger>
-                              <TabsTrigger value="satellite"
-                                           onClick={() => setMapType("satellite")}>Satellite</TabsTrigger>
-                          </TabsList>
-                      </Tabs>
-                  </div>
-              </MapControl>
-              <MapControl position={3}>
-                  <div className={"rounded-lg bg-background m-4 p-1 flex gap-2 outline outline-1 outline-accent"}>
-                      <Button variant={"ghost"} className={"w-10 p-2.5"}
-                              onClick={() => setWindow({...window, zoom: window.zoom - 1})}><Minus/></Button>
-                      <Button variant={"ghost"} className={"w-10  p-2.5"}
-                              onClick={() => setWindow({...window, zoom: window.zoom + 1})}><Plus/></Button>
-                  </div>
-              </MapControl>
+              >
+                  <MapControl position={1}>
+                      <div className={"rounded-lg bg-background m-4 p-1 outline outline-1 outline-accent"}>
+                          <Tabs defaultValue={mapType}>
+                              <TabsList>
+                                  <TabsTrigger value="roadmap"
+                                               onClick={() => setMapType("roadmap")}>Roadmap</TabsTrigger>
+                                  <TabsTrigger value="satellite"
+                                               onClick={() => setMapType("satellite")}>Satellite</TabsTrigger>
+                              </TabsList>
+                          </Tabs>
+                      </div>
+                  </MapControl>
+                  <MapControl position={3}>
+                      <div className={"rounded-lg bg-background m-4 p-1 flex gap-2 outline outline-1 outline-accent"}>
+                          <Button variant={"ghost"} className={"w-10 p-2.5"}
+                                  onClick={() => setWindow({...window, zoom: window.zoom - 1})}><Minus/></Button>
+                          <Button variant={"ghost"} className={"w-10  p-2.5"}
+                                  onClick={() => setWindow({...window, zoom: window.zoom + 1})}><Plus/></Button>
+                      </div>
+                  </MapControl>
+              </GoogleMap>
           </APIProvider>
     );
 }
